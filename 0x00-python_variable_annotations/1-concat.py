9db951371fc8016661274501654fb2832b14e339
kirlos basta <kirlosbasta@gmail.com>
2024-07-04 22:23:09 +0300

    Add 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''1. Basic annotations - concat'''
 
+
 def concat(str1: str, str2: str) -> str:
     '''function that returns a concatenated string'''
     return str1 + str2
