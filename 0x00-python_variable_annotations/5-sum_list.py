647e3220b00c9c8c4bf9826abf66786538975090
kirlos basta <kirlosbasta@gmail.com>
2024-07-04 22:49:33 +0300

    Add 5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''5. Complex types - list of floats'''
+
+
+def sum_list(input_list: list[float]) -> float:
+    '''function that takes a list of floats as argument
+    and returns their sum'''
+    return sum(input_list)
