aa3f0d5f11798100ee4551989344c2a024454960
kirlos basta <kirlosbasta@gmail.com>
2024-07-04 22:39:47 +0300

    Add 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''3. Basic annotations - to string'''
+
+
+def to_str(n: float) -> str:
+    '''function that returns the string representation of the float'''
+    return str(n)
