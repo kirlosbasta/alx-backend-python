85e0f2f56d201b1d9deff38c900a94f08fbbdaa1
kirlos basta <kirlosbasta@gmail.com>
2024-07-04 22:35:47 +0300

    Add 2-floor.py

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''2. Basic annotations - floor'''
 
+
 def floor(n: float) -> int:
     '''function that returns the floor of the float'''
-    return n.__floor__()
+    return int(n)
