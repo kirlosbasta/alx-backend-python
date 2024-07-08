8bd6fd6bf82a24fd0468ba970f6b4a59fe332825
kirlos basta <kirlosbasta@gmail.com>
2024-07-09 02:53:55 +0300

    Add 0-basic_async_syntax.py

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''0. The basics of async'''
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    '''return a random float between 0 and max_delay'''
+    val = random.uniform(0, max_delay)
+    return val
