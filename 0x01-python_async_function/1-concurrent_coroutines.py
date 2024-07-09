03210b207ea95c1d2f46bd5d028b99211828ece1
kirlos basta <kirlosbasta@gmail.com>
2024-07-09 03:58:11 +0300

    Add 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,9 @@ import random
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''return a list of n delay times concurrently'''
     tasks = [wait_random(max_delay) for i in range(n)]
     res = []
     for future in asyncio.as_completed(tasks):
