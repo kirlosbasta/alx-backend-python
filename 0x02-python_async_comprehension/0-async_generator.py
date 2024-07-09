326588c11243dd23cb3f6d16293719ba066359cc
kirlos basta <kirlosbasta@gmail.com>
2024-07-09 21:46:48 +0300

    Add 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,6 +6,7 @@ from typing import AsyncIterator
 
 
 async def async_generator() -> AsyncIterator[float]:
+    '''return an iterator between 0 and 10'''
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
