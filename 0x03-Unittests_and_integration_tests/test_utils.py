767ea2cb9b5c6774794a3c156e997bf300cb0e6d
kirlos basta <kirlosbasta@gmail.com>
2024-07-29 01:24:56 +0300

    Add test_utils.py

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''
+0. Parameterize a unit test
+'''
+import unittest
+from parameterized import parameterized
+from typing import Mapping, Sequence, Any
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    '''Test accress_nested_map function'''
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map: Mapping,
+                               path: Sequence, expected: Any) -> None:
+        '''should return the expected result'''
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, expected)
