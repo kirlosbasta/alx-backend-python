148fed68a8c6035839a6e38afd938fcce80ce1fe
kirlos basta <kirlosbasta@gmail.com>
2024-07-29 23:57:17 +0300

    Add test for puplic repos method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -57,3 +57,14 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(result, ['python', 'php'])
             mock_get_json.assert_called_once()
             mock_url.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo: Dict, license: str,
+                         expected: bool) -> None:
+        '''should return true if has license false otherwise'''
+        test = GithubOrgClient('test')
+        result = test.has_license(repo, license)
+        self.assertEqual(result, expected)
